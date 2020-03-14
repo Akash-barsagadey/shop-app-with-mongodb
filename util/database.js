811f1739bf2131cde927c5b787cc9c8ec72a34d9
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-03-14 15:07:50 +0530

    shop app with mongodb with fetch all products

---
+++ util/database.js
@@ -0,0 +1,29 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://akashbarsagadey:akash12345@cluster0-l7dfp.mongodb.net/test?retryWrites=true&w=majority',
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
